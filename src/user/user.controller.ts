c8eb16fa2c8e41b0478f1fee6d25ebd84c2e32bc
Andrey Veres <spacecmetanka@gmail.com>
2025-04-28 18:24:15 +0300

    feat: create user method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,18 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user-dto';
+import { UserEntity } from './user.entity';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  async createUser(@Body('user') createUserDto: CreateUserDto) {
-    console.log(createUserDto);
-    return this.userService.createUser(createUserDto);
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<{ user: UserEntity }> {
+    const user = await this.userService.createUser(createUserDto);
+
+    return { user };
   }
 }
