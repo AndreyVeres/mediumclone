c6f0c0f9c0e0f1228507283d00f8bdcd26988711
Andrei Veras <andrei_veras@senla.eu>
2025-05-06 16:20:32 +0300

    feat: swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'node:process';
 import { config } from 'dotenv';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('Your API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addServer('https://staging.yourapi.com/', 'Staging')
+    .addServer('https://production.yourapi.com/', 'Production')
+    .addTag('Your API Tag')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.APP_PORT ?? 3000);
 }
 
