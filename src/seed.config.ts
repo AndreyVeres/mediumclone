fbcc8cbc41ef18295121a78f98c3bd6db985a9f6
Andrei Veras <andrei_veras@senla.eu>
2025-05-13 09:57:12 +0300

    fix: update env vars

--- src/seed.config.ts
+++ src/seed.config.ts
@@ -7,7 +7,7 @@ export default new DataSource({
   host: process.env.DB_HOST,
   port: +process.env.POSTGRES_PORT,
   username: process.env.POSTGRES_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
